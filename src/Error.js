5ce8a5525e8a7a4f30b9523552aa63b581b4578c
github username <github email address>
2019-04-15 16:10:09 -0400

    added to error page

--- src/Error.js
+++ src/Error.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {NationalGeographicAPI} from 'national-geographic-api';
 
-const Error = () => {
+class Error extends React.Component {
 
-  return <h1>Error goes here</h1>
+  render() {
+    return (
+      <div>
+        <h1>GO CLEAN UP THE BEACH</h1>
+        <img src="https://www.graziame.com/sites/default/files/graziame/styles/1140_630_wide_landscape/public/images/2019/03/13/dustan-woodhouse-675082-unsplash.jpg?itok=cC7oDM6M"/>
+      </div>
+
+    )
+
+  }
 
 
 }
